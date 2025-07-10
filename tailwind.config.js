3826f64b5b94a26356b98408adcf3b73ecc8dc2c
schevko <schevko@hotmail.com>
2025-07-10 14:58:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    './resources/**/*.blade.php',
+    './resources/**/*.vue',
+    './resources/**/*.js',
+    // Eğer React kullanıyorsan:
+    // './resources/**/*.jsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Özel renk, font vs. ekleyeceksen buraya göm.
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    // Örneğin form, typography eklentisi istersen:
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+  ],
+};
