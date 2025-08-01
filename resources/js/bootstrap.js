d0eff1620e83a018052e576ded54779bbaf275d0
Emir efe karaaslan <efesamsun5561@gmail.com>
2025-08-01 13:41:53 +0300

    file delete

--- resources/js/bootstrap.js
+++
@@ -1,4 +0,0 @@
-// ES6 import ifadesi yerine CommonJS require kullanın
-const axios = require('axios');
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
